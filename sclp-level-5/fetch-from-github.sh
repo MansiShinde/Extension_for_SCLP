ff732777161f25d1465388273caec75b6d5bcbdc
MansiShinde <shinde.mansi55@gmail.com>
2022-08-24 12:12:08 -0400

    Delete sclp-level-5 directory

--- sclp-level-5/fetch-from-github.sh
+++
@@ -1,3 +0,0 @@
-SRC=/home/uday/other-repositories/sclp.git/trunk/Level-4/
-DEST=.
-scp -r $SRC/* $DEST
--- sclp-level-5/regression-testing/test-cases/level3/l3-exmp1.c
+++
@@ -1,9 +0,0 @@
-void main()
-{
-    int num2,greater;
-    int num1;
-    read num1;
-    read num2;
-    greater = num1>=num2 || num1*num2 > num1+num2 ? num1 : num2;    
-    print greater;
-}
